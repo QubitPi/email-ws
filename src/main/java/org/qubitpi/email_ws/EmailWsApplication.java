2844e5defb690e29b14432429b1217def86bf279
Jack <jack20220723@gmail.com>
2025-05-11 19:52:28 +0800

    Project init using fast-spring with 'curl -G http://localhost:8080/starter.zip -d dependencies=web -d javaVersion=17 -d name=email-ws -d groupId=org.qubitpi -d artifactId=email-ws -d packaging=jar -o email-ws.zip'

---
+++ src/main/java/org/qubitpi/email_ws/EmailWsApplication.java
@@ -0,0 +1,13 @@
+package org.qubitpi.email_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qubitpi/email_ws/EmailWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qubitpi.email_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
